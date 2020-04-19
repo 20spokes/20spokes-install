b37d9d58689ff6d7608df5dbc0d275eefe6d678d
Matthew Sells <matthew.m.sells@gmail.com>
2020-04-19 13:22:12 -0500

    Update install script

--- install.sh
+++ install.sh
@@ -1,22 +1,16 @@
 mkdir ~/20spokes-toolkit
 git clone git@github.com:20spokes/20spokes-toolkit.git ~/20spokes-toolkit
-echo 'Source files copied...'
 
 chmod +x ~/20spokes-toolkit/20spokes
-echo 'Updating permissions...'
-
-FILE=~/.zshrc
-if test -f "$FILE"; then
-  echo 'export PATH="$HOME/20spokes-toolkit:$PATH"' >> $FILE
-  echo 'Updating .zshrc...'
-fi
 
 FILE=~/.bash_profile
 if test -f "$FILE"; then
   echo 'export PATH="$HOME/20spokes-toolkit:$PATH"' >> $FILE
-  echo 'Updating .bash_profile...'
+  exec bash
 fi
 
-echo ''
-echo '20spokes Toolkit installed successfully!'
-echo ''
+FILE=~/.zshrc
+if test -f "$FILE"; then
+  echo 'export PATH="$HOME/20spokes-toolkit:$PATH"' >> $FILE
+  exec zsh
+fi
